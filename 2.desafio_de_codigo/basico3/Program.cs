3b3598e53df1fbf3a631a151c76b029865ac1402
fgandraf <fgandraf@gmail.com>
2022-10-04 20:24:28 -0300

    Desafio de codigo - básico 3

---
+++ 2.desafio_de_codigo/basico3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+ 
+class Solution {
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+ 
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                count++;
+            }
+            if (count > n)
+            {
+                Console.WriteLine(false);
+            }
+        }
+         Console.WriteLine(count == 3);
+    }
+}
+
+//Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos.
+//Caso contrário, retorne false. O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m. 
+
+/*
+using System;
+ 
+class Solution {
+    public static void Main(string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+ 
+        for (int i = 1; i <= n; i++) {
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+            if (n % i ==   ) {
+                count++;
+            }
+            if (count >   ) {
+                Console.WriteLine(false);
+            }
+        }
+         Console.WriteLine(count == 3);
+    }
+}
+*/
