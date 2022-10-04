1581b280abcf018402782078b75d9667ce06b022
fgandraf <fgandraf@gmail.com>
2022-10-04 19:48:27 -0300

    Desafio de código - Básico 1

---
+++ 2.desafio_de_codigo/basico1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class DIO
+{
+       
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digito um número");
+        int n = int.Parse(Console.ReadLine());
+        int p = 1;
+        int s = 0;
+        while (n > 0)
+        {
+            int l = n % 10;
+            p *= l;
+            s += l;
+            n /= 10;
+        }
+        Console.WriteLine("Resultado: " + (p-s));
+    }
+
+}
