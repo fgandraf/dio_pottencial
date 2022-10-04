34ee3f6d0573b877b70dcc8d61012c8a2b8626ec
fgandraf <fgandraf@gmail.com>
2022-10-04 20:30:29 -0300

    Desafio de código - básico 3: limpeza do arquivo

--- 2.desafio_de_codigo/basico3/Program.cs
+++ 2.desafio_de_codigo/basico3/Program.cs
@@ -20,28 +20,3 @@ class Solution {
          Console.WriteLine(count == 3);
     }
 }
-
-//Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos.
-//Caso contrário, retorne false. O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m. 
-
-/*
-using System;
- 
-class Solution {
-    public static void Main(string[] args) {
-        int n = int.Parse(Console.ReadLine());
-        int count = 0;
- 
-        for (int i = 1; i <= n; i++) {
-// TODO: Crie as outras condições necessárias para a resolução do desafio:
-            if (n % i ==   ) {
-                count++;
-            }
-            if (count >   ) {
-                Console.WriteLine(false);
-            }
-        }
-         Console.WriteLine(count == 3);
-    }
-}
-*/
