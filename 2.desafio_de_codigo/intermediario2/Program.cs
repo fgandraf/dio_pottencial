44bb82c8d7e8a09f0056a3ebbaae026640f18a5d
fgandraf <fgandraf@gmail.com>
2022-10-13 12:43:22 -0300

    Estrutura do Desafio de Código - Intemediário

--- 2.desafio_de_codigo/intermediario2/Program.cs
+++ 2.desafio_de_codigo/intermediario2/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;  
+
+class minhaClasse { 
+
+  static void Main(string[] args) {  
+
+    double salario = 0.00; 
+    double reajuste = 0.00; 
+    double novoSalario = 0.00; 
+    double percentual = 0.00; 
+  
+    salario = Convert.ToDouble(Console.ReadLine()); 
+
+//TODO: Complete os espaços em branco com uma possível solução para o problema:
+   
+    if( < ) { 
+      return; 
+
+    } else if ( <= ) { 
+      percentual = ; 
+      reajuste =  ; 
+      novoSalario =   ; 
+
+
+    } else if (     <= ) { 
+      percentual =   ; 
+      reajuste =  ;         
+      novoSalario = ; 
+
+    } else if (    <= ) { 
+      percentual = ; 
+      reajuste =  ; 
+      novoSalario = ; 
+
+    } else if (   <= ) { 
+      percentual =  ; 
+      reajuste =   ; 
+      novoSalario =  ; 
+
+    } else { 
+      percentual = ; 
+      reajuste = ; 
+      novoSalario = ; 
+
+    } 
+
+    Console.WriteLine("Novo salario: {0:0.00}",              ); 
+    Console.WriteLine("Reajuste ganho: {0:0.00}",             ); 
+    Console.WriteLine("Em percentual: {0} %", percentual *     ); 
+
+  } 
+}
