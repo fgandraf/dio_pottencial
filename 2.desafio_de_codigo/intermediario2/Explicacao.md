a92ac7f5e20ea4419e23edd58cc5163621acd8a0
fgandraf <fgandraf@gmail.com>
2022-10-14 00:58:19 -0300

    Desfio de código - Intermediário 2

--- 2.desafio_de_codigo/intermediario2/Explicacao.md
+++ 2.desafio_de_codigo/intermediario2/Explicacao.md
@@ -1,4 +1,4 @@
-##Desafio
+# Desafio
 
 A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
  
@@ -14,24 +14,24 @@ Acima de 2000.00     4%
 
 Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
 
-#Entrada
+# Entrada
 
 A entrada contém apenas um valor de ponto flutuante, que pode ser maior ou igual a zero, com duas casas decimais.
 
-#Saída
+# Saída
 
 Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste e o percentual de reajuste ganho, conforme exemplo abaixo.
  
-Exemplo de Entrada 	Exemplo de Saída
+**Exemplo de Entrada** 	**Exemplo de Saída**
 
-400.00              Novo salario: 460.00
-                    Reajuste ganho: 60.00
-                    Em percentual: 15 %
+400.00                  Novo salario: 460.00
+                        Reajuste ganho: 60.00
+                        Em percentual: 15 %
 
-800.01              Novo salario: 880.01
-                    Reajuste ganho: 80.00
-                    Em percentual: 10 %
+800.01                  Novo salario: 880.01
+                        Reajuste ganho: 80.00
+                        Em percentual: 10 %
 
-2000.00             Novo salario: 2140.00
-                    Reajuste ganho: 140.00
-                    Em percentual: 7 %
+2000.00                 Novo salario: 2140.00
+                        Reajuste ganho: 140.00
+                        Em percentual: 7 %
--- 2.desafio_de_codigo/intermediario2/Program.cs
+++ 2.desafio_de_codigo/intermediario2/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;  
+﻿using System;
+using System.Globalization;
 
 class minhaClasse { 
 
   static void Main(string[] args) {  
 
+    CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+    
     double salario = 0.00; 
     double reajuste = 0.00; 
     double novoSalario = 0.00; 
@@ -11,42 +14,45 @@ class minhaClasse {
   
     salario = Convert.ToDouble(Console.ReadLine()); 
 
-//TODO: Complete os espaços em branco com uma possível solução para o problema:
    
-    if( < ) { 
+    if(salario < 0.00)
+    { 
       return; 
-
-    } else if ( <= ) { 
-      percentual = ; 
-      reajuste =  ; 
-      novoSalario =   ; 
-
-
-    } else if (     <= ) { 
-      percentual =   ; 
-      reajuste =  ;         
-      novoSalario = ; 
-
-    } else if (    <= ) { 
-      percentual = ; 
-      reajuste =  ; 
-      novoSalario = ; 
-
-    } else if (   <= ) { 
-      percentual =  ; 
-      reajuste =   ; 
-      novoSalario =  ; 
-
-    } else { 
-      percentual = ; 
-      reajuste = ; 
-      novoSalario = ; 
-
+    }
+    else if (salario <= 400.00)
+    { 
+      percentual = 0.15; 
+      reajuste =  salario * percentual; 
+      novoSalario = salario + reajuste; 
+    }
+    else if (salario <= 800.00)
+    { 
+      percentual = 0.12; 
+      reajuste =  salario * percentual; 
+      novoSalario = salario + reajuste; 
+    }
+    else if (salario <= 1200.00)
+    { 
+      percentual = 0.10; 
+      reajuste =  salario * percentual; 
+      novoSalario = salario + reajuste; 
+    }
+    else if (salario <= 2000.00)
+    { 
+      percentual = 0.07; 
+      reajuste =  salario * percentual; 
+      novoSalario = salario + reajuste; 
+    } else
+    { 
+      percentual = 0.04; 
+      reajuste =  salario * percentual; 
+      novoSalario = salario + reajuste; 
     } 
 
-    Console.WriteLine("Novo salario: {0:0.00}",              ); 
-    Console.WriteLine("Reajuste ganho: {0:0.00}",             ); 
-    Console.WriteLine("Em percentual: {0} %", percentual *     ); 
+    Console.WriteLine("Novo salario: {0:0.00}", novoSalario); 
+    Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste); 
+    Console.WriteLine("Em percentual: {0:0} %", percentual * 100); 
+
 
   } 
 }
