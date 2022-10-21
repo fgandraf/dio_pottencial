03e9e97d35de3a876927012e80817aedaf42350e
fgandraf <fgandraf@gmail.com>
2022-10-21 19:15:21 -0300

    CRUD Completo

--- Exercicios/Exercicio_ASP/Models/ErrorViewModel.cs
+++ Exercicios/Exercicio_ASP/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Exercicio_ASP.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
