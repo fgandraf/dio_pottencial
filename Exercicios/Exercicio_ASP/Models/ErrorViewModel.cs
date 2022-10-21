82b5413fb66c1c706d944da07aec9fed8045f183
fgandraf <fgandraf@gmail.com>
2022-10-21 16:04:03 -0300

    Exercício Asp.NET

---
+++ Exercicios/Exercicio_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Exercicio_ASP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
